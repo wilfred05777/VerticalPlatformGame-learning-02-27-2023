4349c0c4748a1137a444d0ba4cd5cfc82e01f7c3
Wilfred <wilfred05777@gmail.com>
2023-02-27 18:15:13 +0800

    21:56mins

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 // @ts-nocheck
+
+// https://www.youtube.com/watch?v=rTVoyWu8r6g
+
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
@@ -6,12 +9,23 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
-c.fillStyle = 'white'
-c.fillRect(0, 0, canvas.width, canvas.height)
+// c.fillStyle = 'white'
+// c.fillRect(0, 0, canvas.width, canvas.height)
 
-c.fillStyle = 'red'
-c.fillRect(200, 100, 100, 100)
+// c.fillStyle = 'red'
+// c.fillRect(200, 100, 100, 100)
+
+let y = 100
 
 function animate() {
-  window.requestAnimationFrame
+  window.requestAnimationFrame(animate)
+
+  c.fillStyle = 'white'
+  c.fillRect(0, 0, canvas.width, canvas.height)
+
+  c.fillStyle = 'red'
+  c.fillRect(200, y, 100, 100)
+  y++
 }
+
+animate()
